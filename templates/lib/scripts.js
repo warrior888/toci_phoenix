acf47f9e757bff75be253070c98c0787bc1f43fd
Chmura <romduz@gmail.com>
2015-07-16 13:03:33 +0200

    MG up to 1 PM

--- templates/lib/scripts.js
+++ templates/lib/scripts.js
@@ -18,6 +18,22 @@ function sendAJAX(url,callback,elementIDToReplace){
 
     });
 
+}
 
+
+function setSize(){
+
+var docHeight = $(document).height();
+    var docWidth = $(document).width();
+
+    var style = {height:docHeight, width: docWidth};
+
+var viewports = document.getElementsByClassName("viewport");
+
+    for(var i in viewports){
+
+        $(viewports[i]).css(style); //cos zle
+        console.log(viewports[i]);
+    }
 
 }
