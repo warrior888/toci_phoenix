cd7fd130bba8b9d70c09f6cd1d8272bcd3fad474
Rudi <1010000i100@gmail.com>
2015-07-17 21:28:13 +0000

    nowe okno dodania klienta

--- projects/shark/Shark/Shark/MainMenu.cs
+++ projects/shark/Shark/Shark/MainMenu.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Shark
 {
-    public partial class Form1 : Form
+    public partial class MainMenu : Form
     {
-        public Form1()
+        public MainMenu()
         {
             InitializeComponent();
+        }
+
+        private void newClient_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
