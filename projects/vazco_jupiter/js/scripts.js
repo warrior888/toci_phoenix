5891f1eff8c639f7ffe4b0cb1c9a815c74c21e42
mg <garbacz.m@hotmail.com>
2015-07-20 10:48:06 +0200

    index uncompleted

---
+++ projects/vazco_jupiter/js/scripts.js
@@ -0,0 +1,35 @@
+
+
+function AboutLinkClick() {
+    GetHtmlByAjax("dane/aboutCourse.html", insertHTML, "centralContent");
+}
+
+function HowItWorksLinkClick() {
+
+    GetHtmlByAjax("dane/howItWorks.html", insertHTML,"centralContent");
+}
+
+function OrganisersLinkClick() {
+
+    GetHtmlByAjax("dane/organisers.html", insertHTML,"centralContent");
+}
+
+function FAQLinkClick() {
+
+    GetHtmlByAjax("dane/faq.html", insertHTML,"centralContent");
+}
+
+function ApplyLinkClick() {
+
+    GetHtmlByAjax("dane/apply.html", insertHTML,"centralContent");
+}
+
+function ContactLinkClick() {
+
+    GetHtmlByAjax("dane/contact.html", insertHTML,"centralContent");
+}
+
+function About2Click() {
+
+    GetHtmlByAjax("dane/about2.html", insertHTML,"centralContent");
+}
