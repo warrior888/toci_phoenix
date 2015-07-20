4449326f50fd053505bbc5fbaf3107bdf70fe7b2
Rudi <1010000i100@gmail.com>
2015-07-20 12:24:51 +0200

    zmiana id w apply.html aby było zgodne ze skryptem, implode dla Db

--- projects/vazco_jupiter/server/Db.php
+++ projects/vazco_jupiter/server/Db.php
@@ -1,10 +1,15 @@
 ﻿<?php
 
+require_once 'DbHandle.php';
+
 	class Db
 	{
 		public function Save($table, $items)
 		{
 			//generate insert
 			//DbHandle Query
+			$insert = 'insert into '.$table. " (".implode(array_keys(',',$items)).") values ".implode(',',array_values($items)).")";
+
+			$this->DbHandle->Query($query);
 		}
 	}
--- projects/vazco_jupiter/server/apply.php
+++ projects/vazco_jupiter/server/apply.php
@@ -1,1 +1,24 @@
-﻿
+﻿<?php
+
+require_once "Db.php";
+
+
+$dbTable='applicants';
+
+/* Nazwy z formularza: narazie sa inne,
+ * applicantName
+ * applicantSurname
+ * applicantEmail
+ * applicantPhone
+ */
+
+
+
+$applicant['name'] = $_POST['applicantName'];
+$applicant['surname'] = $_POST['applicantSurname'];
+$applicant['email'] = $_POST['applicantEmail'];
+$applicant['phone'] = $_POST['applicantPhone'];
+
+$db=new Db();
+
+$db->Save($dbTable,$applicant);
