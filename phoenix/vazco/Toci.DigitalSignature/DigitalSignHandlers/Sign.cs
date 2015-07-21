3700716a4205a75474e76993856823f098ba3717
TypowyAdam <adam0309@gmail.com>
2015-07-21 13:54:40 +0200

    Minor updates

--- phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Sign.cs
+++ phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Sign.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Toci.DigitalSignature.Interfaces;
 
 namespace Toci.DigitalSignature.DigitalSignHandlers
@@ -13,46 +11,20 @@ namespace Toci.DigitalSignature.DigitalSignHandlers
     {
         protected const string crypthoAlgorithm = "RSA";
 
-        public static string CertificateToString(X509Certificate2 certificate)
-        {
-
-            using (MemoryStream ms = new MemoryStream())
-            {
-                new BinaryFormatter().Serialize(ms, certificate);
-                return Convert.ToBase64String(ms.ToArray());
-            }
-        }
-
-        public static X509Certificate2 StringToCertifcate(string base64String)
-        {
-            byte[] bytes = Convert.FromBase64String(base64String);
-            using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
-            {
-                ms.Write(bytes, 0, bytes.Length);
-                ms.Position = 0;
-                return (X509Certificate2) new BinaryFormatter().Deserialize(ms);
-            }
-        }
-
         public virtual byte[] SignFile(byte[] inputFile, X509Certificate2 certificate)
         {
             if ( (inputFile.Equals(null)  || !certificate.HasPrivateKey)) return default(byte[]);
 
             var algorithmName = certificate.SignatureAlgorithm.FriendlyName.Replace(crypthoAlgorithm, String.Empty);
             var privateKey = (RSACryptoServiceProvider) certificate.PrivateKey;
-            RSACryptoServiceProvider privateKey1 = new RSACryptoServiceProvider();
-            privateKey1.ImportParameters(privateKey.ExportParameters(true));
+            if (!algorithmName.Contains("sha1") || !algorithmName.Contains("md5"))
+            {
+                RSACryptoServiceProvider privateKey1 = new RSACryptoServiceProvider();
+                privateKey1.ImportParameters(privateKey.ExportParameters(true));
+                privateKey = privateKey1;
+            }
             byte[] hash = HashAlgorithm.Create(algorithmName).ComputeHash(inputFile);
-            return privateKey1.SignHash(hash, CryptoConfig.MapNameToOID(algorithmName));
-        }
-        public virtual byte[] SignFile(byte[] inputFile, string base64String)
-        {
-            X509Certificate2 certificate = StringToCertifcate(base64String);
-
-
-            
-            return SignFile(inputFile, certificate);
-
+            return privateKey.SignHash(hash, CryptoConfig.MapNameToOID(algorithmName));
         }
     }
 }
