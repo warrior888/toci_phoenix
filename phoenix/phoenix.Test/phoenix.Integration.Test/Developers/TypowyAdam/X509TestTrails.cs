83fa018d616eb29ea1d604de2bcdc6252813398c
TypowyAdam <adam0309@gmail.com>
2015-07-21 09:33:07 +0200

    test

--- phoenix/phoenix.Test/phoenix.Integration.Test/Developers/TypowyAdam/X509TestTrails.cs
+++ phoenix/phoenix.Test/phoenix.Integration.Test/Developers/TypowyAdam/X509TestTrails.cs
@@ -1,20 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Toci.DigitalSignature.DigitalSignHandlers;
 
 namespace Phoenix.Integration.Test.Developers.TypowyAdam
 {
     [TestClass]
     public class X509TestTrails
     {
+        string certSubject = "CertyfikatTestowy";
+        string testMaterial = "asdasdasd";
+        Dictionary<string, byte[]> hashList = new Dictionary<string, byte[]>();
+
         [TestMethod]
         public void SignTest()
         {
+            X509Store my = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            my.Open(OpenFlags.ReadOnly);
+            RSACryptoServiceProvider csp = null;
+            Sign signProvider = new Sign();
+            
+            foreach (X509Certificate2 cert in my.Certificates)
+            {
+                if (cert.Subject.Contains(certSubject))
+                {
+                    hashList.Add(cert.Subject,signProvider.SignFile(Encoding.ASCII.GetBytes(testMaterial), cert));
 
+                }
+            }
         }
 
         [TestMethod]
-        public void VerifyTest()
+        public void VerifyTest(byte[] verifyHash)
         {
+            X509Store my = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            my.Open(OpenFlags.ReadOnly);
+            RSACryptoServiceProvider csp = null;
+            Verify verifyProvider = new Verify();
+            var listOfFiles = Directory.GetFiles(@"C:\CertTest");
+            foreach (var dictionaryEntity in hashList)
+            {
+                foreach (var path in listOfFiles)
+                {
+                    X509Certificate2 cert = new X509Certificate2(path);
+                    if (cert.Subject.Contains(dictionaryEntity.Key))
+                    {
+                        verifyProvider.VerifyFile(Encoding.ASCII.GetBytes(testMaterial), dictionaryEntity.Value, cert);
+                    }
+                }
+
+                
+            }
 
         }
     }
