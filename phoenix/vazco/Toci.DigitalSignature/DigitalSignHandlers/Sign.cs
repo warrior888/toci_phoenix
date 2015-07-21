326abf4217e6121549765c5ff2627728a427d9ad
szyfr4nt <Nirvana007@wp.pl>
2015-07-21 12:21:00 +0200

    Just need testify it

--- phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Sign.cs
+++ phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Sign.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -9,7 +11,27 @@ namespace Toci.DigitalSignature.DigitalSignHandlers
     public class Sign: ISign
     {
         protected const string crypthoAlgorithm = "RSA";
-     
+
+        public static string CertificateToString(X509Certificate2 certificate)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, certificate);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public static X509Certificate2 StringToCertifcate(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
+            {
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Position = 0;
+                return (X509Certificate2) new BinaryFormatter().Deserialize(ms);
+            }
+        }
+
         public virtual byte[] SignFile(byte[] inputFile, X509Certificate2 certificate)
         {
             if ( (inputFile.Equals(null)  || !certificate.HasPrivateKey)) return default(byte[]);
@@ -38,6 +60,20 @@ namespace Toci.DigitalSignature.DigitalSignHandlers
             */
             return privateKey.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
             
+        }
+        public virtual byte[] SignFile(byte[] inputFile, string base64String)
+        {
+            X509Certificate2 certificate = StringToCertifcate(base64String);
+
+            if ((inputFile.Equals(null) || !certificate.HasPrivateKey)) return default(byte[]);
+
+            var algorithmName = certificate.SignatureAlgorithm.FriendlyName;//Replace(crypthoAlgorithm, String.Empty);
+            
+            var privateKey = (RSACryptoServiceProvider)certificate.PrivateKey;
+            byte[] hash = HashAlgorithm.Create("SHA1").ComputeHash(inputFile);
+            
+            return privateKey.SignHash(hash, CryptoConfig.MapNameToOID("SHA1"));
+
         }
     }
 }
--- phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Verify.cs
+++ phoenix/vazco/Toci.DigitalSignature/DigitalSignHandlers/Verify.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using Toci.DigitalSignature.Interfaces;
@@ -9,12 +11,45 @@ namespace Toci.DigitalSignature.DigitalSignHandlers
     {
         protected const string crypthoAlgorithm = "RSA";
 
+
+        public static string CertificateToString(X509Certificate2 certificate)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                new BinaryFormatter().Serialize(ms, certificate);
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        public static X509Certificate2 StringToCertifcate(string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+            using (MemoryStream ms = new MemoryStream(bytes, 0, bytes.Length))
+            {
+                ms.Write(bytes, 0, bytes.Length);
+                ms.Position = 0;
+                return (X509Certificate2)new BinaryFormatter().Deserialize(ms);
+            }
+        }
+
         public bool VerifyFile(byte[] inputFile, byte[] signature, X509Certificate2 certificate)
         {
             if (inputFile.Equals(null) || signature == null) return false;
             var algorithmName = certificate.SignatureAlgorithm.FriendlyName.Replace(crypthoAlgorithm, String.Empty).ToUpper();
             var publicKey = (RSACryptoServiceProvider)certificate.PublicKey.Key;
             
+            byte[] hash = HashAlgorithm.Create(algorithmName).ComputeHash(inputFile);
+            return publicKey.VerifyHash(hash, CryptoConfig.MapNameToOID(algorithmName), signature);
+        }
+
+        public bool VerifyFile(byte[] inputFile, byte[] signature, string base64String)
+        {
+            X509Certificate2 certificate = StringToCertifcate(base64String);
+
+            if (inputFile.Equals(null) || signature == null) return false;
+            var algorithmName = certificate.SignatureAlgorithm.FriendlyName.Replace(crypthoAlgorithm, String.Empty).ToUpper();
+            var publicKey = (RSACryptoServiceProvider)certificate.PublicKey.Key;
+
             byte[] hash = HashAlgorithm.Create(algorithmName).ComputeHash(inputFile);
             return publicKey.VerifyHash(hash, CryptoConfig.MapNameToOID(algorithmName), signature);
         }
