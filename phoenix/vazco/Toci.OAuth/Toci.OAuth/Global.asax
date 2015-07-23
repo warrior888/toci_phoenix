6b4b1fefdb293c8204f0f686df4f4ed7ad5db0ad
TypowyAdam <adam0309@gmail.com>
2015-07-23 10:33:12 +0200

    OAuth project

---
+++ phoenix/vazco/Toci.OAuth/Toci.OAuth/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Toci.OAuth.MvcApplication" Language="C#" %>
---
+++ phoenix/vazco/Toci.OAuth/Toci.OAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Toci.OAuth.Startup))]
+namespace Toci.OAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
