d88743a43cee67271297a53b83bf5da24c2feb56
szyfr4nt <Nirvana007@wp.pl>
2015-07-28 12:45:29 +0200

    no message

--- phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Provider/TociAuthenticationExtensions.cs
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Provider/TociAuthenticationExtensions.cs
@@ -1,7 +1,58 @@
-﻿namespace Toci.Provider
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Facebook;
+using Owin;
+
+
+namespace Toci.Provider
 {
-    public class TociAuthenticationExtensions
+    /// <summary>
+    /// Extension methods for using <see cref="FacebookAuthenticationMiddleware"/>
+    /// </summary>
+    public static class TociAuthenticationExtensions
     {
-         
+        /// <summary>
+        /// Authenticate users using Facebook
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder"/> passed to the configuration method</param>
+        /// <param name="options">Middleware configuration options</param>
+        /// <returns>The updated <see cref="IAppBuilder"/></returns>
+        public static IAppBuilder UseTociAuthentication(this IAppBuilder app, TociAuthenticationOptions options)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException("app");
+            }
+            if (options == null)
+            {
+                throw new ArgumentNullException("options");
+            }
+
+            app.Use(typeof(TociAuthenticationMiddleware), app, options);
+            return app;
+        }
+
+        /// <summary>
+        /// Authenticate users using Facebook
+        /// </summary>
+        /// <param name="app">The <see cref="IAppBuilder"/> passed to the configuration method</param>
+        /// <param name="appId">The appId assigned by Facebook</param>
+        /// <param name="appSecret">The appSecret assigned by Facebook</param>
+        /// <returns>The updated <see cref="IAppBuilder"/></returns>
+        public static IAppBuilder UseTociAuthentication(
+            this IAppBuilder app,
+            string appId,
+            string appSecret)
+        {
+            return UseTociAuthentication(
+                app,
+                new TociAuthenticationOptions
+                {
+                    AppId = appId,
+                    AppSecret = appSecret,
+                });
+        }
     }
 }
