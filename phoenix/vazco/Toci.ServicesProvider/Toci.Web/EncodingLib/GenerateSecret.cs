cbfff7db0f115e37a09147268d0cabda1beb3fab
TypowyAdam <adam0309@gmail.com>
2015-07-29 12:33:58 +0200

    Możliwość wstawienia własnego salta

--- phoenix/vazco/Toci.ServicesProvider/Toci.Web/EncodingLib/GenerateSecret.cs
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/EncodingLib/GenerateSecret.cs
@@ -13,7 +13,7 @@ namespace EncodingLib
         private byte[] _salt;
         private byte[] _hash;
         private byte[] _hashBytes = new byte[64];
-
+        
         private GenerateSecret()
         {
             new RNGCryptoServiceProvider().GetBytes(_salt = new byte[32]);
@@ -21,6 +21,17 @@ namespace EncodingLib
 
         public GenerateSecret(string password) : this()
         {
+            var pbkdf2 = new Rfc2898DeriveBytes(password, _salt, 10000);
+            _hash = pbkdf2.GetBytes(32);
+            Array.Copy(_salt, 0, _hashBytes, 0, 32);
+            Array.Copy(_hash, 0, _hashBytes, 32, 32);
+        }
+
+        public GenerateSecret(string password, byte[] salt) //czy to w ogóle jest potrzebne?
+        {
+            _salt = new byte[32];
+            _hash = new byte[32];
+            Array.Copy(salt, 0, _salt, 0, salt.Length<32?salt.Length:32);
             var pbkdf2 = new Rfc2898DeriveBytes(password, _salt, 10000);
             _hash = pbkdf2.GetBytes(32);
             Array.Copy(_salt, 0, _hashBytes, 0, 32);
