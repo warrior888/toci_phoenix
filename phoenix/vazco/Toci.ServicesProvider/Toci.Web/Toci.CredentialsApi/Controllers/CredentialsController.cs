9202046b654456df1aa1712dc41c918018a37b61
Duch <kniedziela00@gmail.com>
2015-07-29 14:18:11 +0200

    Refactor Ultimate Master Disaster Eilogue

---
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.CredentialsApi/Controllers/CredentialsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Toci.CredentialsApi.Controllers
+{
+    public class CredentialsController : ApiController
+    {
+        [Route("Credentials/Save")]
+        ///[HttpPost]
+        [HttpGet]
+        public void SaveCredentials(string data)
+        {
+            
+        }
+    }
+}
