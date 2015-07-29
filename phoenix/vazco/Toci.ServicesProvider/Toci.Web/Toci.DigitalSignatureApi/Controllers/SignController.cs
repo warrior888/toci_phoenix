62e6a878d5728a1b48679b0b038b2c25f5f94472
TypowyAdam <adam0309@gmail.com>
2015-07-29 15:45:43 +0200

    DigitalSignatureApi

---
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.DigitalSignatureApi/Controllers/SignController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Toci.DigitalSignature.DigitalSignHandlers;
+
+namespace Toci.DigitalSignatureApi.Controllers
+{
+    public class SignController : ApiController
+    {
+        [HttpGet]
+        [Route("/Sign")]
+        public string Sign(string base64Data, string base64Certyficate)
+        {
+            var sign = new Sign();
+            return Convert.ToBase64String(sign.SignFile(base64Data, base64Certyficate));
+        }
+    }
+}
