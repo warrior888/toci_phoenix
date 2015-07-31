58751cb17ca61f071eba0a5ce0ba431e1e81a656
mg <garbacz.m@hotmail.com>
2015-08-01 01:38:49 +0200

    video responsywne , zrobiony loader, inna koncepcja mapy. TODO: autoplay video bo coś nie banlga, FAQ, sprawdzenie scrollspy

--- projects/mg-jupiter/js/DocumentReady.js
+++ projects/mg-jupiter/js/DocumentReady.js
@@ -8,51 +8,24 @@ $(document).ready(function()
         {
             autoPlay: 3000,
             lazyLoad : true,
-            items:3,
+            responsive: {
 
-
-
-
+                0: {
+                    items: 1
+                },
+                760: {
+                    items: 2
+                }
+            }
         }
-
-
     );
     window.onresize =changeArrowOnResize;
 
 
     google.maps.event.addDomListener(window, 'load', initMap);
+
+    turnLoaderOff();
+
 });
 
 
-function initMap(){
-
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
-
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(50.6672560, 17.9048559),
-
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":66},{"color":"#001100"},{"lightness":60}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
-    };
-
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('mapa');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(50.6672560, 17.9048559),
-        map: map,
-        title: 'Toci !'
-    });
-
-
-}
