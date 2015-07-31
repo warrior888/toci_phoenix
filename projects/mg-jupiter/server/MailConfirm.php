207393fbe4f323c281c001dccc1151156ecbdd96
Ghost Rider <warriorr@poczta.fm>
2015-07-31 10:03:05 +0200

    mail confirm

---
+++ projects/mg-jupiter/server/MailConfirm.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+	class MailConfirm 
+	{
+		$salt = 'fnduwoghwoghweoghwghwdiguohwergo';
+
+		public function CreateSignature($mail)
+		{
+			$signature = md5($mail.$this->salt.date('h:i:s'));
+
+			// save to db signature
+			//generate link confirm.php?signature=$signature
+
+			// wsylac na $mail
+		}
+
+		public function Verify($sign)
+		{
+			/// get from db
+
+			// updaye e ten mail jest potwierdzony
+		}
+	}
