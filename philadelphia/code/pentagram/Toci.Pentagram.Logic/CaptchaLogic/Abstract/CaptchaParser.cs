96fcfe9112aead8b87856cc8647b18bfbd0673cb
Vernathic <natalia.legarska@gmail.com>
2015-08-02 22:10:27 +0200

    Add Pentagram project + some architecture

---
+++ philadelphia/code/pentagram/Toci.Pentagram.Logic/CaptchaLogic/Abstract/CaptchaParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toci.Pentagram.Logic.CaptchaLogic.Abstract
+{
+	abstract class CaptchaParser
+	{
+		public abstract string ConvertToBase64(string codeSnippet);
+
+		// private methods:
+		// TODO: string codeSnippet -> png
+		// TODO: png -> base64
+		// TODO: return it to front end
+
+		// jak się oznacza klasy abstrakcyjne? CaptchaParserAbstract? xD
+	}
+}
---
+++ philadelphia/code/pentagram/Toci.Pentagram.Logic/CaptchaLogic/Logic/CaptchaLogic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toci.Pentagram.Logic.CaptchaLogic.Abstract;
+
+namespace Toci.Pentagram.Logic.CaptchaLogic.Logic
+{
+	class CaptchaLogic : CaptchaParser
+	{
+		public override string ConvertToBase64(string codeSnippet)
+		{
+			// TODO: implement
+			return "";
+		}
+
+		// private methods:
+		// TODO: string codeSnippet -> png
+		// TODO: png -> base64
+		// TODO: return it to front end
+	}
+}
