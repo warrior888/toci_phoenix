f1ee50609da6fda5b6126e732fabe39c6b69b0a7
TypowyAdam <adam0309@gmail.com>
2015-08-03 20:01:23 +0200

    Odebrałem tokena :D

---
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Client/Models/VazcoTokenModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Toci.Client.Models
+{
+    public class VazcoTokenModel
+    {
+        public string token_type;
+        public string access_token;
+        public string expires_in;
+    }
+}
