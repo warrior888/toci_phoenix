3a797e0d2f0d415e594dabc5477716e325f7fcb7
Terry <wysocki31@gmail.com>
2015-08-03 12:01:02 +0200

    sls menu fix

--- Orders/slsconsulting/website/js/scrolling-nav.js
+++ Orders/slsconsulting/website/js/scrolling-nav.js
@@ -9,11 +9,27 @@ $(window).scroll(function() {
 
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
+
+    var navHeight = $("#navigation-bar").outerHeight();
+    var navSectionOffset = 25;
+
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        var docOffset = $(window).scrollTop();
+        var sectionOffset;
+
+        console.log(docOffset);
+        if (docOffset >= 270) {
+            sectionOffset = navHeight + navSectionOffset;
+        } else {
+            sectionOffset = navHeight + navSectionOffset + 90; //rozkminic: jak wyci¹gn¹æ tê liczbê?
+        }
+        console.log(sectionOffset);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: ($($anchor.attr('href')).offset().top - sectionOffset)
+            }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
+
+
