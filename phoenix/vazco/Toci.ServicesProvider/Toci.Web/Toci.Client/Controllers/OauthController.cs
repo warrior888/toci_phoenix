1596840372520f772c8efdaa6b89f4187e075a24
TypowyAdam <adam0309@gmail.com>
2015-08-04 21:59:03 +0200

    no message

--- phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Client/Controllers/OauthController.cs
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Client/Controllers/OauthController.cs
@@ -21,55 +21,81 @@ namespace Toci.Client.Controllers
 {
     public class OauthController : Controller
     {
+        private const string AccessCode = "code";
+        private const string RedirectUri = "redirect_uri";
+        private const string ClientSecret = "client_secret";
+        private const string ClientId = "client_id";
+        private const string GrantType = "grant_type";
+        private const string AuthorizationCode = "authorization_code";
+        private const string VazcoAuthorizeUri = "http://oauth.stg.vazco.eu/oauth2/authorize";
+        private const string LocalAuthenticationUri = "http://localhost:13188/oauth/authentication";
+        private const string AppId = "testoauth";
+        private const string AppSecret = "testoauth";
+        private const string VazcoGetIdentityUri = "http://oauth.stg.vazco.eu/oauth2/getIdentity";
+        private const string VazcoTokenUri = "http://oauth.stg.vazco.eu/oauth2/token";
+
         public ActionResult Index()
         {
             string VazcoAuthUri =
-                String.Format(
-                    "{0}?response_type={1}&client_id={2}&redirect_uri={3}", "http://oauth.stg.vazco.eu/oauth2/authorize",
-                    "code", "testoauth", "http://localhost:13188/oauth/authentication"); 
-
-
+                string.Format(
+                    "{0}?response_type={1}&client_id={2}&redirect_uri={3}", VazcoAuthorizeUri,
+                    AccessCode, AppId, LocalAuthenticationUri); 
 
             return Redirect(VazcoAuthUri);
         }
-        // GET: Oauth
-        //[HttpPost]
+
         [Route("/oauth/authentication")]
         public async Task<RedirectToRouteResult> Authentication(string code)
         {
+            var responseModel = await GetResultOfConnectionToVazco(code);
+            //Skaczemy albo do strony logowania albo do rejestracji nowego konta
+            if (await IsRegistred(responseModel.id))
+            {
+                return RedirectToAction("Login", "Account", new LoginViewModel() { Email = responseModel.email });
+            }
+
+            TempData["responseModel"] = responseModel; //TODO czy da się to wypieprzyć?
+            return RedirectToAction("RegisterForm");
+        }
+
+        private async Task<VazcoResponseModel> GetResultOfConnectionToVazco(string code)
+        {
+            var token = await GetOauthToken(code);
+            var identity = await GetUserIdentity(token);
+            var responseModel = ConvertToResponseModel(identity, token);
+            //^^ na tym etapie mamy informacje o użytkowniku 
+            return responseModel;
+        }
+
+        private async Task<VazcoTokenModel> GetOauthToken(string code)
+        {
             var requestClient = new HttpClient();
-            var Params = new Dictionary<string,string>()
-                {
-                    {"grant_type", "authorization_code" },
-                    {"code", code },
-                    {"client_id", "testoauth" },
-                    {"client_secret", "testoauth" },
-                    {"redirect_uri", "http://localhost:13188/oauth/authentication" }
-                };
+            var Params = new Dictionary<string, string>()
+            {
+                {GrantType, AuthorizationCode},
+                {AccessCode, code},
+                {ClientId, AppId},
+                {ClientSecret, AppSecret},
+                {RedirectUri, LocalAuthenticationUri}
+            };
             var content = new FormUrlEncodedContent(Params);
-            var response = await requestClient.PostAsync("http://oauth.stg.vazco.eu/oauth2/token", content);
-
+            var response = await requestClient.PostAsync(VazcoTokenUri, content);
             var responseString = await response.Content.ReadAsStringAsync();
+            var jsonToken = (JObject) JsonConvert.DeserializeObject(responseString);
+            var token = (VazcoTokenModel) jsonToken.ToObject(typeof (VazcoTokenModel));
+            return token;
+        }
 
-
-            var jsonToken = (JObject)JsonConvert.DeserializeObject(responseString);
-            var token = (VazcoTokenModel)jsonToken.ToObject(typeof (VazcoTokenModel));
+        private async Task<VazcoIdentityModel> GetUserIdentity(VazcoTokenModel token)
+        {
             var identityClient = new HttpClient();
             identityClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.access_token);
-            HttpResponseMessage identityResponseMessage = await identityClient.GetAsync("http://oauth.stg.vazco.eu/oauth2/getIdentity");
+            HttpResponseMessage identityResponseMessage =
+                await identityClient.GetAsync(VazcoGetIdentityUri);
             var readContent = await identityResponseMessage.Content.ReadAsStringAsync();
             var jsonIdentity = (JObject) JsonConvert.DeserializeObject(readContent);
-            var identity = (VazcoIdentityModel)jsonIdentity.ToObject(typeof(VazcoIdentityModel));
-            //^^ na tym etapie mamy informacje o użytkowniku 
-            //Skaczemy albo do strony logowania albo do rejestracji nowego konta
-            if (await IsRegistred(identity.id))
-            {
-                return RedirectToAction("Login", "Account", new LoginViewModel() { Email =identity.email });
-            }
-            var responseModel = ConvertToResponseModel(identity, token);
-            TempData["responseModel"] = responseModel;
-            return RedirectToAction("RegisterForm");
-
+            var identity = (VazcoIdentityModel) jsonIdentity.ToObject(typeof (VazcoIdentityModel));
+            return identity;
         }
 
 
@@ -77,6 +103,14 @@ namespace Toci.Client.Controllers
         {
             return new VazcoResponseModel() {id = identity.id, email = identity.email, access_token = token.access_token, token_type = token.token_type, expires_in = token.expires_in};
         }
+
+        private async Task<bool> IsRegistred(string id)
+        {
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var result = await userManager.FindByIdAsync(id);
+            return result != null;
+        }
+
         [HttpGet]
         public ViewResult RegisterForm()
         {
@@ -89,25 +123,23 @@ namespace Toci.Client.Controllers
         {
 
             var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            /*var result = await userManager.FindByIdAsync(id);
-            if (result != null)
-            {
-                return RedirectToAction("Login", "Account", new LoginViewModel() {Email = email});
-            }*/
+
             ApplicationUser user = new ApplicationUser() { Id = id, UserName = email, Email = email }; //do uzupełnienia jeśli dojdą nowe claimsy
-            //var signInManager = HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+
             var makeUser = await userManager.CreateAsync(user, password);
             if (makeUser.Succeeded)
             {
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 await SignIn(user.UserName, password);
-
+                //RETURN x.succeeded? jedna metoda : druga metoda
                 return RedirectToAction("Index", "Home");
             }
             return RedirectToAction("ExternalLoginFailure", "Account", makeUser.Errors.First());
+
         }
 
-        //
+        
+
         private async Task<bool> SignIn(string userName, string password)
         {
             var signInManager = HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
@@ -121,13 +153,5 @@ namespace Toci.Client.Controllers
             }
             return true;
         }
-
-        private async Task<bool> IsRegistred(string id)
-        {
-            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var result = await userManager.FindByIdAsync(id);
-            return result != null;
-        }
-
     }
 }
