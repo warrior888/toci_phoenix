e8dcede0b7d9ca78791fd6edcedb2512ed697258
Patryk Stulczewski <patrykj122@o2.pl>
2015-08-05 18:33:29 +0200

    hint do tematu wiadomosci

--- Orders/slsconsulting/website/js/servicesLoading.js
+++ Orders/slsconsulting/website/js/servicesLoading.js
@@ -1,5 +1,18 @@
+
+var subjects = {
+    'car-rentals'  : 'import/export',
+    'car-rentals1': 'sprzedaz samochodow',
+    'car-rentals2' : 'wypozyczalnia samochodow',
+    'car-rentals3' : 'sprzet rolniczy',
+    'car-rentals4' : 'transport miedzynarodowy',
+    'car-rentals5' : 'wazywa/owoce',
+    'car-rentals6' : 'dzial IT',
+    'car-rentals7' : 'domki caloroczne'
+};
+
+
 $( document ).ready(function() {
-    /*AppendOnClick2('car-rentals', 'servicesContainer', 'server/Content/car-renting.html');
+    AppendOnClick2('car-rentals', 'servicesContainer', 'server/Content/car-renting.html');
     //AppendOnClick('return', 'servicesContainer', 'server/Content/services.html');
 
     AppendOnClick2('car-rentals1', 'servicesContainer', 'server/Content/car-renting.html');
@@ -21,7 +34,7 @@ $( document ).ready(function() {
     //AppendOnClick('return', 'servicesContainer', 'server/Content/services.html');
 
     AppendOnClick2('car-rentals7', 'servicesContainer', 'server/Content/car-renting.html');
-    //AppendOnClick('return', 'servicesContainer', 'server/Content/services.html');*/
+    //AppendOnClick('return', 'servicesContainer', 'server/Content/services.html');
 });
 
 
@@ -29,5 +42,11 @@ function AppendOnClick(actionTrgId, containerId, fileName) {
     $('#'+ actionTrgId).on("click", function(e){
             e.preventDefault();
             $('#'+containerId).hide().load(fileName).fadeIn(1500);
+    });
+}
+
+function AppendOnClick2(actionTrgId, containerId, fileName) {
+    $('#'+ actionTrgId).on("click", function(e){
+        $('#met_subject').val(subjects[actionTrgId]);
     });
 }
