28f2737e4258c16c6ebf09a79c0fc9532d874711
wilddance <1010000i100@gmail.com>
2015-08-05 22:08:46 +0200

    aby włączyć cache trzeba zainstalować Memcached dla php, zmiana na false aby nikomu nie wywalało

--- philadelphia/code/server/Application/config.php
+++ philadelphia/code/server/Application/config.php
@@ -3,7 +3,7 @@
 $config = array();
 
 $config['connection_string'] = 'host=46.101.222.238 user=postgres password=q1 dbname=postgres';
-$config['useCache']=true;
+$config['useCache']=false;
 $config['MEMCACHED_HOST']="127.0.0.1";
 $config['MEMCACHED_PORT']="11211";
 
