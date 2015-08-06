9fca49117faabfadf7d18df3a8c20af947c5ed05
Terry <wysocki31@gmail.com>
2015-08-06 15:26:26 +0200

    no message

--- Orders/slsconsulting/website/js/init.js
+++ Orders/slsconsulting/website/js/init.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
    // $('#home-section').load('server/content/home.html');
    // $('#about-section').load('server/content/about.html');
-    $("#services-slider").owlCarousel();
-    $('#servicesContainer').load('server/content/services-slider.html');
+    //$("#services-slider").owlCarousel();
+
+
+    $('#servicesContainer').load('server/content/services.html');
 
 
 
