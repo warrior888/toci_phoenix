bdbdf96e23aa9235590a4a03b1ee78c63ecbd065
szyfr4nt <Nirvana007@wp.pl>
2015-08-06 10:30:57 +0200

    db working

--- phoenix/vazco/Toci.ServicesProvider/Toci.Web/DbCredentials/Certificate/CertConfig.cs
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/DbCredentials/Certificate/CertConfig.cs
@@ -3,6 +3,6 @@
     public static class CertConfig
     {
         public static string privateKeySecret = "P@ssw0rd";
-        public static string certPath = @"\..\..\DbCredentials\Certificate\TestPrywatnegoKlucza.pfx";
+        public static string certPath = @"\..\..\..\DbCredentials\Certificate\Certyfikat.pfx";
     }
 }
