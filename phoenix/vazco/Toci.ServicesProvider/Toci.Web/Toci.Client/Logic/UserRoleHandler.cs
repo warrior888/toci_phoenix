0abb06dee93da3d17a3fec5d7a8dcdc476d2e53a
TypowyAdam <adam0309@gmail.com>
2015-08-06 11:50:44 +0200

    UserRoleUtils

---
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.Client/Logic/UserRoleHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Toci.Client.Models;
+
+namespace Toci.Client.Logic
+{
+    public class UserRoleHandler
+    {
+        public async Task<bool> AddNewUserRole(DbContext context, string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            //IdentityResult roleResult; //<-jakby się okazało że IdentityResult przechowuje jakieś ważne/przydatne dalej dane
+
+            // Check to see if Role Exists, if not create it
+            if (!roleManager.RoleExists(roleName))
+            {
+                await roleManager.CreateAsync(new IdentityRole(roleName));
+            }
+            return roleManager.RoleExists(roleName);
+        }
+
+        public async Task<bool> AddRoleToUser(DbContext context, string userId, string roleName)
+        {
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+            var user = userManager.FindByIdAsync(userId);
+            if (user != null && roleManager.RoleExists(roleName))
+            {
+                await userManager.AddToRoleAsync(userId, roleName);
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
