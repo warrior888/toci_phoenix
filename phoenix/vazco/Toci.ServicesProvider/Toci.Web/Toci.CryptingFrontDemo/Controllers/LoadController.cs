ef04db2838c19a99931cf86f20ef753f7310689a
Duch <kniedziela00@gmail.com>
2015-08-07 10:09:43 +0200

    crypting api prototype

--- phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.CryptingFrontDemo/Controllers/LoadController.cs
+++ phoenix/vazco/Toci.ServicesProvider/Toci.Web/Toci.CryptingFrontDemo/Controllers/LoadController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Toci.CryptingApi.Controllers;
+using Toci.CryptingApi.Models;
 
 namespace Toci.CryptingFrontDemo.Controllers
 {
     public class LoadController : Controller
     {
-        // GET: Load
-        public ActionResult Load()
+        private const string InvalidPasswordMsg = "Invalid password!";
+
+        [HttpPost]
+        public ActionResult Load(BodyModel model)
         {
-            return View();
+            var dbLoad = new DbLoadController();
+            var dbList = dbLoad.LoadDbModels(model).Where(x => x.data != InvalidPasswordMsg);
+            if (model.password == default(string)) dbList = default(IEnumerable<BodyModel>);
+            return View(    dbList     );
         }
     }
 }
