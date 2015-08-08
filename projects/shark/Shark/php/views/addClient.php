5a75e17956122094d410f5724e49c71e7550988f
Rudi <1010000i100@gmail.com>
2015-08-08 08:02:16 +0200

    addClient

---
+++ projects/shark/Shark/php/views/addClient.php
@@ -0,0 +1,18 @@
+
+<form action="addClient.php" method="post">
+    <table>
+    <pre>
+    <tr><td>Nazwa</td><td> <input type="text" name="nazwa"></td></tr>
+    <tr><td>Ulica</td><td> <input type="text" name="ulica"></td></tr>
+    <tr><td>Kod pocztowy</td><td> <input type="text" name="kodPocztowy"></td></tr>
+    <tr><td>Miasto</td><td> <input type="text" name="miasto"></td></tr>
+    <tr><td>NIP</td><td> <input type="text" name="nip"></td></tr>
+    <tr><td></td><td align="right"><input type="submit" value="taak"></td>
+    </pre>
+    </table>
+</form>
+
+
+
+<?php
+
--- projects/shark/Shark/php/views/logged_in.php
+++ projects/shark/Shark/php/views/logged_in.php
@@ -1,8 +1,10 @@
+
+
+
+
 <?php echo $_SESSION['user_name']; ?>
-co tam
 
-
-
+<a href="print.html"  onclick="window.open('views/addClient.php', 'newwindow', 'width=300, height=250'); return false;"> Dodaj klienta</a>
 
 
 <a href="index.php?logout">Logout</a>
