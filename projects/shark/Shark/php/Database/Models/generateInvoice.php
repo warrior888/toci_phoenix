86de0027cd0201f541fe36b746d47ad8f18ee737
Rudi <1010000i100@gmail.com>
2015-08-09 20:17:54 +0200

    kilka drobnostek do sharka

--- projects/shark/Shark/php/Database/Models/generateInvoice.php
+++ projects/shark/Shark/php/Database/Models/generateInvoice.php
@@ -4,6 +4,8 @@ include "../ClientManager.php";
 include_once "../ServiceManager.php";
 include_once "../../config.php";
 
+
+
     if(isset($_GET['client'])&&
         isset($_GET['service'])
     //    isset($_GET['number'])
@@ -19,8 +21,6 @@ include_once "../../config.php";
 
         $number="02/02/0202";
 
-        //var_dump($client);
-
         $nabywca="Imie i nazwisko/Tytuł: ".$client['name']."<br />Adres: ".$client['address']."<br />kod pocztowy: ".$client['postalcode'].
             "<br />Miasto: ".$client['city']."<br />NIP:".$client['nip'];
 
@@ -34,5 +34,51 @@ include_once "../../config.php";
      <tr><td rowspan="2">NABYWCA:<br />' . $nabywca . '</td>
         </tr><tr></tr><tr><td colspan="2">koncowa linia</td></tr></table>';
 
-        echo $invoice;
+        echo $invoice+"<br/><br/>";
+
+
+
+
+        $table= '
+<table>
+  <tr>
+    <th>Lp.</th>
+    <th>Nazwa</th>
+    <th>Ilość</th>
+    <th>Cena <br />jedn.<br />(zł.)</th>
+    <th>VAT<br />(%)</th>
+    <th>Kwota<br />VAT(zł.)</th>
+    <th>Wartość<br />Netto(zł.)</th>
+    <th>Wartość<br />Brutto(zł.)</th>
+  </tr>
+  <tr>
+    <td>11</td>
+    <td>22</td>
+    <td>33</td>
+    <td>44</td>
+    <td>55</td>
+    <td>66</td>
+    <td>77</td>
+    <td>88</td>
+  </tr>
+  <tr>
+    <td colspan="5">999</td>
+    <td>111</td>
+    <td>222</td>
+    <td></td>
+  </tr>
+</table>
+        ';
+
+
+
+
+
+
+
+
+
+
+
+
     }
