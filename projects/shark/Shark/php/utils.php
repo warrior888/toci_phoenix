97405b42184b75fb0177b78810daa367987445f1
Rudi <1010000i100@gmail.com>
2015-08-10 14:31:03 +0200

    Generowanie numer faktury done

--- projects/shark/Shark/php/utils.php
+++ projects/shark/Shark/php/utils.php
@@ -4,15 +4,37 @@ include "Database/InvoiceManager.php";
 
 function getMonth()
 {
-    var_dump(date_parse(getdate()['month'])['month']);
+    return date_parse(getdate()['month'])['month'];
 }
 
 function getNextInvoiceNumber(){
     $manager=new InvoiceManager();
-    $lastInvoiceNumber=intval($manager->GetInvoice("max(id)")[0]['max']);
-    var_dump($lastInvoiceNumber);
-    //$invoice
-    //$numbers=explode("/",)
+    $numberOfLastInvoice=explode("/",$manager->GetInvoice("number","id=".intval($manager->GetInvoice("max(id)")[0]['max']))[0]["number"]);
+    $newNumber=array();
+
+    //sprawdzamy miesiac z ostatniej faktury
+    if(intval($numberOfLastInvoice[1])==getMonth()){
+        //jeśli nie jest zgodny z aktualnym miesiacem, pobieramy numer ostatniej faktury i dodajemy jeden
+        $newNumber[0]=intval($numberOfLastInvoice[0])+1;
+    }else{
+        //albo ustawiamy 1, jako pierwsza faktura miesiaca
+        $newNumber[0]=01;
+    }
+
+    //aktualny miesiac
+    $newNumber[1]=intval(getMonth());
+
+    //nowy rok narazie bez implementacji
+    $newNumber[2]=2015;
+
+    return implode("/",$newNumber);
 }
 
-getNextInvoiceNumber();
+function getTodaysDate()
+{
+    return getdate()['mday']."/".getMonth()."/".getdate()['year'];
+}
+
+$manager=new InvoiceManager();
+$numberOfLastInvoice=explode("/",$manager->GetInvoice("number","id=".intval($manager->GetInvoice("max(id)")[0]['max']))[0]["number"]);
+
