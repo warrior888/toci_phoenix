8b76b8bc258eba07fe370fd9dff76c744cd535c3
szyfr4nt <Nirvana007@wp.pl>
2015-08-14 14:06:47 +0200

    no message

---
+++ IdentityServerAndClient/Toci.Identity/Toci.Identity/Clients/MvcFormPostClient/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens;
+
+namespace MvcFormPostClient
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "Cookies"
+                });
+
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    AuthenticationType = "TempCookie",
+                    AuthenticationMode = AuthenticationMode.Passive
+                });
+        }
+    }
+}
