ea3117fdaf2ae486a3c99c7fc852aae9bcc18106
UCantTouchThis <adam.stawarz11@gmail.com>
2015-08-17 12:23:33 +0200

    fix

--- toci_common/Toci.Libraries/Toci.Db/DbVirtualization/Model.cs
+++ toci_common/Toci.Libraries/Toci.Db/DbVirtualization/Model.cs
@@ -92,13 +92,20 @@ namespace Toci.Db.DbVirtualization
             }
         }
 
-        public void SetSelect(string columnName, SelectClause clause)
+        public IModel SetSelect(string columnName, SelectClause clause)
         {
             if (Fields.ContainsKey(columnName))
             {
                 Fields[columnName].SetWhere(true);
                 Fields[columnName].SetSelectClause(clause);
             }
+            return this;
+        }
+
+        public IModel SetSelect<T>(string columnName, SelectClause clause, T value)
+        {
+            SetValue(columnName, value);
+            return SetSelect(columnName, clause);
         }
 
         public List<IModel> GetDataRowsList(DataSet table)
