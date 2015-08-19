c9409eddb3b443692fed7246847589a2a6b4f850
UCantTouchThis <adam.stawarz11@gmail.com>
2015-08-19 14:51:18 +0200

    Magic

--- Toci.Libraries/Toci.Utilities/Api/ApiSimpleResultManager.cs
+++ Toci.Libraries/Toci.Utilities/Api/ApiSimpleResultManager.cs
@@ -3,13 +3,13 @@ using Toci.Utilities.Common.Api;
 
 namespace Toci.Utilities.Api
 {
-    public class ApiSimpleResultManager : ApiResultManager<string, SimpleResult>
+    public class ApiSimpleResultManager : ApiResultManager<object, SimpleResult>
     {
         private ApiJsonSerializer _serializer = new ApiJsonSerializer();
 
-        public override Dictionary<string, string> GetApiResult(SimpleResult result, string serializationFormat) // parametr serializacji slownika data
+        public override Dictionary<string, object> GetApiResult(SimpleResult result, string serializationFormat) // parametr serializacji slownika data
         {
-            var apiResult = new Dictionary<string, string>
+            var apiResult = new Dictionary<string, object>
             {
                 { "code", result.Code.ToString() },
                 { "message", result.Message },
@@ -17,7 +17,7 @@ namespace Toci.Utilities.Api
             };
             if (result.Data != null)
             {
-                apiResult.Add("data", _serializer.GetJson(result.Data));
+                apiResult.Add("data", result.Data);
             }
             if (result.Code > 0)
             {
