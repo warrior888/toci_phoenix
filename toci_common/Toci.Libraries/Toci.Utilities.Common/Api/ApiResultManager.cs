f6a22954b9a3d3b2d3e5585ea66496fa7c654dfa
warrior888 <warriorr@poczta.fm>
2015-08-19 10:00:28 +0200

    3 things :P

---
+++ toci_common/Toci.Libraries/Toci.Utilities.Common/Api/ApiResultManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Toci.Utilities.Common.Api
+{
+    public abstract class ApiResultManager<TInner, TResult>
+    {
+        public abstract Dictionary<string, TInner> GetApiResult(TResult result, string serializationFormat);
+    }
+}
---
+++ toci_common/Toci.Libraries/Toci.Utilities/Api/ApiJsonSerilizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Toci.Utilities.Api
+{
+    public class ApiJsonSerializer
+    {
+        //private const string _pattern = "{0}";
+        private const string _patternElement = "\"{0}\": {1}";
+        private const string _delimiter = ",";
+        public virtual string GetJson(Dictionary<string, string> elements)
+        {
+            var entries = elements.Select(item => string.Format(_patternElement, elements.Keys, elements.Values));
+
+            return "{" + string.Join(_delimiter, entries) + "}";
+        }
+    }
+}
