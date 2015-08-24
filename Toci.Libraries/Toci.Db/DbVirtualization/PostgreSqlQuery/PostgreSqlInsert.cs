b764e2e3a95735ba3a48e89fdbaacb84b47112e8
Duch <kniedziela00@gmail.com>
2015-08-24 12:03:48 +0200

    dupa

--- Toci.Libraries/Toci.Db/DbVirtualization/PostgreSqlQuery/PostgreSqlInsert.cs
+++ Toci.Libraries/Toci.Db/DbVirtualization/PostgreSqlQuery/PostgreSqlInsert.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Toci.Db.Interfaces;
 
 namespace Toci.Db.DbVirtualization.PostgreSqlQuery
--- Toci.Libraries/Toci.Utilities.Interfaces/Toci.Utilities.Interfaces/User/IPosition.cs
+++ Toci.Libraries/Toci.Utilities.Interfaces/Toci.Utilities.Interfaces/User/IPosition.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Toci.Utilities.Interfaces.User.Privileges;
 
 namespace Toci.Utilities.Interfaces.User
