b7f17ec8ee330c6cd9dfafb915f1934cf14a5918
UCantTouchThis <adam.stawarz11@gmail.com>
2015-08-28 14:01:28 +0200

    Dynamic creation of a class example

---
+++ DynamicClass/DynamicClass/MainClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+namespace DynamicClass
+{
+    public class MainClass
+    {
+        public string Magic()
+        {
+            var codeToExecute = PocUtil.GetStringFromFile(PocUtil.Path);
+
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+
+            CompilerParameters parameters = new CompilerParameters
+            {
+                GenerateInMemory = true,
+                GenerateExecutable = false 
+            };
+
+            CompilerResults compilerResults = provider.CompileAssemblyFromSource(parameters, codeToExecute);
+
+            var types = compilerResults.CompiledAssembly.DefinedTypes;
+
+            dynamic instanceFromcompilerResults = Activator.CreateInstance(types.FirstOrDefault().AsType());
+
+            return instanceFromcompilerResults.MethodOne();
+        }
+    }
+}
---
+++ DynamicClass/DynamicClass/PocDynamicClass.cs
@@ -0,0 +1,12 @@
+﻿namespace DynamicClass
+{
+    public class PocDynamicClass
+    {
+        private const string message = "Test message";
+
+        public string  MethodOne()
+        {
+            return message;
+        }
+    }
+}
