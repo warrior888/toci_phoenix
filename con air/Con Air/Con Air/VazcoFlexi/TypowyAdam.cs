16c8c76398862fe02acc57a1467f68764419e08f
warrior888 <warriorr@poczta.fm>
2015-08-29 10:00:05 +0200

    :)

--- con air/Con Air/Con Air/VazcoFlexi/TypowyAdam.cs
+++ con air/Con Air/Con Air/VazcoFlexi/TypowyAdam.cs
@@ -6,15 +6,24 @@ using System.Threading.Tasks;
 
 namespace Con_Air
 {
-    public class TypowyAdam
+    public class TypowyAdam // Terry
     {
         public void FlexiSexi()
         {
+            string x = "{0}";
+
             string FlexiClass = "class fjdwghrwug {" +
                                 "}" +
                                 "";
 
             //  gutt
+
+            
+        }
+
+        public void RelaxMapping()
+        {
+            
         }
     }
 }
