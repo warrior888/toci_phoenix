f0682b7a0dfd97ebfd95f9ec49aea9d6342c5c97
Patryk Stulczewski <patrykj122@o2.pl>
2015-08-29 12:53:48 +0200

    load class from assembly

---
+++ con air/Con Air/Con Air/Patryk/PatrykLoadClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+namespace Con_Air.Patryk
+{
+    public class PatrykLoadClass
+    {
+        public object Load(string dllPath, string className)
+        {
+            Assembly assembly = Assembly.Load(dllPath);
+
+            //var assembly = Assembly.LoadFrom(dllPath); //??
+
+            Type MyLoadClass = assembly.GetType(string.Format("{0}.{1}", dllPath, className));
+            return Activator.CreateInstance(MyLoadClass);
+
+            //return Activator.CreateInstance(Type.GetType("DALL.LoadClass, DALL", true));
+        }
+    }
+}
