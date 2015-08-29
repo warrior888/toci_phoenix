fff19511af843d7eee1e0c5adcef98299416c8b7
Terry <wysocki31@gmail.com>
2015-08-29 11:11:49 +0200

    no message

---
+++ con air/Con Air/Con Air/Terry/GeneratedClass.cs
@@ -0,0 +1,33 @@
+﻿using System.CodeDom;
+using System.CodeDom.Compiler;
+using Microsoft.CSharp;
+
+namespace Con_Air.Terry
+{
+    public class GeneratedClass
+    {
+        public string GetGeneratedClass()
+        {
+            return "public class TestClass { private string msg = \"Hejka\"" +
+                   " public string GetMsg() {return msg}" +
+                   "}";
+        }
+
+
+        public void Compile(string body)
+        {
+
+            CSharpCodeProvider provider = new CSharpCodeProvider();
+            CompilerParameters cp = new CompilerParameters();
+            
+            cp.ReferencedAssemblies.Add("System.dll");
+            cp.GenerateExecutable = false;
+            cp.GenerateInMemory = true;
+            
+
+            CompilerResults cr = provider.CompileAssemblyFromFile(cp, GetGeneratedClass());
+
+        }
+
+    }
+}
