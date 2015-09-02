36c0957e7257c844c6f8dbaed90aea2eac11c6df
S2yfr4nt <Nirvana007@wp.pl>
2015-09-02 12:40:57 +0200

    fix

--- philadelphia/philadelphia - stable/code/toci.pentagram.interfaces/IDbLogic.cs
+++
@@ -1,9 +0,0 @@
-﻿using Toci.Db.Interfaces;
-
-namespace toci.pentagram.interfaces
-{
-    public interface IDbLogic
-    {
-        IDbHandle GetDbHandle(string user, string password, string dbAddress, string dbName);
-    }
-}
