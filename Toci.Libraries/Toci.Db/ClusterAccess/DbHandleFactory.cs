e383cfd689d1cb5213f75b8debf60c9dcf289248
S2yfr4nt <Nirvana007@wp.pl>
2015-09-02 09:09:29 +0200

    dbhadlefactory refactor

--- Toci.Libraries/Toci.Db/ClusterAccess/DbHandleFactory.cs
+++ Toci.Libraries/Toci.Db/ClusterAccess/DbHandleFactory.cs
@@ -1,4 +1,8 @@
-﻿using Toci.Db.Clients;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Runtime.Remoting.Messaging;
+using Toci.Db.Clients;
 using Toci.Db.DbVirtualization.MsSqlQuery;
 using Toci.Db.DbVirtualization.PostgreSqlQuery;
 using Toci.Db.Interfaces;
@@ -7,17 +11,46 @@ namespace Toci.Db.ClusterAccess
 {
     public static class DbHandleFactory
     {
+        private static Dictionary<SqlClientKind, Func<DbConfig, IDbHandle>> dbHandleFactory
+            = new Dictionary<SqlClientKind, Func<DbConfig, IDbHandle>>
+            {
+                {
+                    SqlClientKind.MsSql, (config) => new DbHandle(
+                        new MsSqlClient(config.UserName, config.Password, config.DbAddress, config.DbName),
+                        new MsSqlSelect(), new MsSqlInsert(), new MsSqlUpdate(), new MsSqlDelete())
+                },
+                {
+                    SqlClientKind.PostgreSql, (config) => new DbHandle(
+                        new PostgreSqlClient(config.UserName, config.Password, config.DbAddress, config.DbName), 
+                        new PostgreSqlSelect(), new PostgreSqlInsert(), new PostgreSqlUpdate(), new PostgreSqlDelete())
+                }
+            };
+
         public static IDbHandle GetHandle(SqlClientKind kind, string name, string password, string dbAddress, string dbName)
         {
-            switch (kind)
+            DbConfig config = new DbConfig
             {
-                case SqlClientKind.MsSql:
-                    return new DbHandle(new MsSqlClient(name, password, dbAddress, dbName), new MsSqlSelect(), new MsSqlInsert(), new MsSqlUpdate(), new MsSqlDelete());
-                case SqlClientKind.PostgreSql:
-                    return new DbHandle(new PostgreSqlClient(name, password, dbAddress, dbName), new PostgreSqlSelect(), new PostgreSqlInsert(), new PostgreSqlUpdate(), new PostgreSqlDelete());
-                default:
-                    return null;
-            }
+                UserName = name,
+                Password = password,
+                DbAddress = dbAddress,
+                DbName = dbName
+            };
+            return GetHandle(kind, config);
+            //Stara wersja kodu zosaje zakomentowana, poniewaz nie ma testów 
+            //            switch (kind)
+            //            {
+            //                case SqlClientKind.MsSql:
+            //                    return new DbHandle(new MsSqlClient(name, password, dbAddress, dbName), new MsSqlSelect(), new MsSqlInsert(), new MsSqlUpdate(), new MsSqlDelete());
+            //                case SqlClientKind.PostgreSql:
+            //                    return new DbHandle(new PostgreSqlClient(name, password, dbAddress, dbName), new PostgreSqlSelect(), new PostgreSqlInsert(), new PostgreSqlUpdate(), new PostgreSqlDelete());
+            //                default:
+            //                    return null;
+            //            }
+        }
+
+        public static IDbHandle GetHandle(SqlClientKind kind, DbConfig config)
+        {
+            return dbHandleFactory.ContainsKey(kind) ? dbHandleFactory[kind](config): null;
         }
     }
 }
