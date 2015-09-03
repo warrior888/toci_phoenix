5c1178cf5013c4f8b6e2c7f04807a319cabf9254
S2yfr4nt <Nirvana007@wp.pl>
2015-09-03 14:36:42 +0200

    no message

---
+++ phoenix/vazco/Toci.Credenatials_v2.0/DbCredentials.Test/DbTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DbCredentials.Test
+{
+    [TestClass]
+    public class DbTests
+    {
+        [TestMethod]
+        public void ModelsGenerator()
+        {
+
+        }
+    }
+}
