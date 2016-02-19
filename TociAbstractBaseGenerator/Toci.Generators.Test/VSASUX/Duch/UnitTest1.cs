7f145d1263a7c21c5447587590a9698f99467960
warrior888 <warriorr@poczta.fm>
2016-02-19 22:59:28 +0100

    no message

---
+++ TociAbstractBaseGenerator/Toci.Generators.Test/VSASUX/Duch/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Generators.Test.VSASUX.Duch
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: TODO
+            //
+        }
+    }
+}
---
+++ TociAbstractBaseGenerator/Toci.Generators.Test/VSASUX/TypowyAdam/TestClass.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Toci.Generators.Test.VSASUX.TypowyAdam
+{
+    [TestClass]
+    public class TestClass
+    {
+         
+    }
+}
